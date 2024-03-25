2ee45225fd220de0ffd44873be356bd644576187
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-03-25 16:40:22 -0300

    req 1 fix

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express';
-import { productService } from '../services';
+import services from '../services';
+
+const { productService } = services;
 
 async function createProduct(req: Request, res: Response): Promise<Response> {
   const { name, price, userId } = req.body;
-  const { status, data } = await services.productService.createProduct({ name, price, userId });
+  const { status, data } = await productService.createProduct({ name, price, userId });
   return res.status(status).json(data);
 } 
 
 async function getAllProducts(req: Request, res: Response): Promise<Response> {
-  const { status, data } = await services.productService.getAllProducts();
+  const { status, data } = await productService.getAllProducts();
   return res.status(status).json(data);
 }
 
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,9 +2,9 @@ import productService from './product.service';
 // import userService from './user.service';
 // import loginService from './login.service';
 
-// export default {
-//   productService,
-// //   userService,
-// //   loginService,  
-// };
-export { productService };
+export default {
+  productService,
+//   userService,
+//   loginService,  
+};
+// export { productService };
